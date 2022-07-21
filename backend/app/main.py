a077a8273486259dd1305a0db67d79be9ead1a07
Sehyun Choi <choisehyun98@gmail.com>
2022-07-21 14:22:16 +0900

    format

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, request
 from flask_cors import CORS
-import random
 import pandas as pd
 
 app = Flask(__name__)  #opening up app
@@ -13,6 +12,7 @@ df = pd.read_csv('../data/data.csv')
 #https://www, if we have /generate, send our data to the url
 APP_ROOT = os.getenv('APP_ROOT', '/generate')
 
+
 @app.route(APP_ROOT, methods=["POST"])
 def generate_pastpaper():
     data = request.json
@@ -22,22 +22,12 @@ def generate_pastpaper():
 
     component2 = topic_df[topic_df['component'] == 2]
     component4 = topic_df[topic_df['component'] == 4]
-    component6 = topic_df[topic_df['component']== 6]
-    
-    images = {
-        'component2':component2['screenshot_path'].to_list(),
-        'component4':component4['screenshot_path'].to_list(),
-        'component6':component6['screenshot_path'].to_list()
+    component6 = topic_df[topic_df['component'] == 6]
 
+    images = {
+        'component2': component2['screenshot_path'].to_list(),
+        'component4': component4['screenshot_path'].to_list(),
+        'component6': component6['screenshot_path'].to_list()
     }
 
-    
-
-
-    
-
-
-
-
-    
     return images
