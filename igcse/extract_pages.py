6af3f1adbf610a748cfe8dca8f2f5e21edd43f57
Sehyun Choi <choisehyun98@gmail.com>
2022-07-25 00:26:54 +0900

    extract pages for long answer questions

---
+++ igcse/extract_pages.py
@@ -0,0 +1,26 @@
+import os
+from pdf2image import convert_from_path
+
+save_path = '../screenshots'
+os.makedirs(save_path, exist_ok=True)
+
+month_map = {'m': 'march', 's': 'summer', 'w': 'winter'}
+
+for year in range(2010, 2022):
+    for f in os.listdir(f'pastpapers/{year}/qp/'):
+        fname = f'pastpapers/{year}/qp/{f}'
+
+        if 'qp_2' in fname and year >= 2016:
+            continue
+
+        _, month, _, comp = f.split('_')
+        month = month_map[month[0]]
+        comp = comp.replace('.pdf', '')
+
+        image_dir = f'{save_path}/{year}/{month}/component{comp}'
+        os.makedirs(image_dir, exist_ok=True)
+
+        pages = convert_from_path(fname)
+
+        for i, page in enumerate(pages):
+            page.save(f'{image_dir}/page' + str(i) + '.png', 'PNG')
