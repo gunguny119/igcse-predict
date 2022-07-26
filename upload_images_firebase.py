c75e29d9370cb6efc949383055ead5b3f56885b9
gunyguny <gunguny119@gmail.com>
2022-07-26 13:41:19 +0900

    upload

--- upload_images_firebase.py
+++ upload_images_firebase.py
@@ -1,3 +1,6 @@
+import os
+from re import L
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
@@ -9,5 +12,12 @@ bucket = storage.bucket()
 
 filename = "screenshots/2010/march/component2/q1.png"
 
-blob = bucket.blob(filename)
-blob.upload_from_filename(filename)
+for year in range(2016, 2022):
+    for month in ['march', 'summer', 'winter']:
+        for comp in os.listdir(f'screenshots/{year}/{month}'):
+            if not os.path.isdir(f'screenshots/{year}/{month}/{comp}'):
+                continue
+            for question in os.listdir(f'screenshots/{year}/{month}/{comp}'):
+                filename = f'screenshots/{year}/{month}/{comp}/{question}'
+                blob = bucket.blob(filename)
+                blob.upload_from_filename(filename)
