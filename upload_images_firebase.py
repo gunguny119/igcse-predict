0f3a0613345857a16253749aa1d739b3777b7595
gunyguny <gunguny119@gmail.com>
2022-07-26 13:41:57 +0900

    upload2

--- upload_images_firebase.py
+++ upload_images_firebase.py
@@ -1,5 +1,5 @@
 import os
-from re import L
+
 
 import firebase_admin
 from firebase_admin import credentials
@@ -9,8 +9,6 @@ cred = credentials.Certificate('igcse-predict-c18eb87031fd.json')
 firebase_admin.initialize_app(cred, {'storageBucket': 'igcse-predict.appspot.com'})
 
 bucket = storage.bucket()
-
-filename = "screenshots/2010/march/component2/q1.png"
 
 for year in range(2016, 2022):
     for month in ['march', 'summer', 'winter']:
