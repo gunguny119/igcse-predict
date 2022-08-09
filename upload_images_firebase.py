3c2abb8efb3ee7dc4047d137ba032b8008bd76fd
Sehyun Choi <choisehyun98@gmail.com>
2022-08-09 13:49:20 +0900

    (rename) / upload marking schemes

--- upload_images_firebase.py
+++ upload_images_firebase.py
@@ -9,15 +9,33 @@ firebase_admin.initialize_app(cred, {'storageBucket': 'igcse-predict.appspot.com
 
 bucket = storage.bucket()
 
+ms = False  # if true, upload marking scheme
+
+if ms:
+    folder = 'marking_schemes'
+else:
+    folder = 'screenshots'
+
 for year in range(2016, 2022):
     for month in ['march', 'summer', 'winter']:
-        for comp in os.listdir(f'screenshots/{year}/{month}'):
-            if not os.path.isdir(f'screenshots/{year}/{month}/{comp}'):
+        for comp in os.listdir(f'{folder}/{year}/{month}'):
+            if not os.path.isdir(f'{folder}/{year}/{month}/{comp}'):
                 continue
-            for question in os.listdir(f'screenshots/{year}/{month}/{comp}'):
-                filename = f'screenshots/{year}/{month}/{comp}/{question}'
+            for question in os.listdir(f'{folder}/{year}/{month}/{comp}'):
+                # m1_1.png  -> q1_p1.png
+                # q1.png -> q1_p1.png
+                # renamed_question = question.replace('m', 'q')
+                # renamed_question = renamed_question.replace('_ppp', '_p')
+                # if not '_' in question:
+                #     renamed_question = renamed_question.split('.')[0] + '_p1.png'
+
+                filename = f'{folder}/{year}/{month}/{comp}/{question}'
+                # renamed_filename = f'{folder}/{year}/{month}/{comp}/{renamed_question}'
                 if question.startswith('page'):
                     continue
+
+                # os.rename(filename, renamed_filename)
+                # continue
 
                 blob = bucket.blob(filename)
                 if blob.exists():
