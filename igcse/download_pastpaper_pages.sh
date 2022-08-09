97d02baffe391b2eef74ddaa43f78a03e64560ba
Sehyun Choi <choisehyun98@gmail.com>
2022-08-09 21:13:55 +0900

    update for extracting ms too

--- igcse/download_pastpaper_pages.sh
+++ igcse/download_pastpaper_pages.sh
@@ -12,7 +12,12 @@ command="sh pdf2txt.sh $subject $code_id ${components[@]}"
 eval $command
 echo finished extracting texts from pastpapers.
 
-echo extracting 1 page screenshots...
+echo "extracting 1 page screenshots for question papers..."
 command="python extract_pages.py --subject $subject"
+eval $command
+echo finished extracting 1 page screenshots.
+
+echo "extracting 1 page screenshots for marking schemes..."
+command="python extract_pages.py --subject $subject --ms"
 eval $command
 echo finished extracting 1 page screenshots.
--- igcse/extract_pages.py
+++ igcse/extract_pages.py
@@ -4,19 +4,21 @@ from pdf2image import convert_from_path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--subject', type=str, default='chemistry')
+parser.add_argument('--ms', action='store_true')
 args = parser.parse_args()
 
-if args.subject == 'chemistry':
-    save_path = '../screenshots'
-else:
-    save_path = f'../screenshots/{args.subject}'
+save_path = 'marking_schemes' if args.ms else 'screenshots'
+if args.subject != 'chemistry':
+    save_path = f'{save_path}/{args.subject}'
+
 os.makedirs(save_path, exist_ok=True)
 
 month_map = {'m': 'march', 's': 'summer', 'w': 'winter'}
 
 for year in range(2016, 2022):
-    for f in os.listdir(f'pastpapers/{args.subject}/{year}/qp/'):
-        fname = f'pastpapers/{args.subject}/{year}/qp/{f}'
+    folder = 'ms' if args.ms else 'qp'
+    for f in os.listdir(f'pastpapers/{args.subject}/{year}/{folder}/'):
+        fname = f'pastpapers/{args.subject}/{year}/{folder}/{f}'
 
         _, month, _, comp = f.split('_')
         month = month_map[month[0]]
