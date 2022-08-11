f03ea64cbca05f9f574bad0ccebb5a8b0909e68c
gunyguny <gunguny119@gmail.com>
2022-08-11 15:25:58 +0900

    fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -187,7 +187,7 @@ export default {
               "Content-Type": "application/json; charset=utf-8",
             },
             body: JSON.stringify({
-              subject: this.selected_subject,
+              subject: this.selected_subjects,
               topics: this.selected_topics,
               options: this.option_map[this.selected_option],
             }),
