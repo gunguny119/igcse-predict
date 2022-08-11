8d0ac0d06b3c4424c6583a587ecc005c615d634d
Sehyun Choi <choisehyun98@gmail.com>
2022-08-11 13:23:04 +0900

    fix

--- pmt/download_pmt.py
+++ pmt/download_pmt.py
@@ -4,31 +4,19 @@ import re
 import requests
 ############################# Change Here ######################################
 TOPIC_LIST = [
-    "particulate-nature-of-matter",
-    "experimental-techniques",
-    "atoms-elements-compounds",
-    "stoichiometry",
-    "electricity-and-chemistry",
-    "chemical-energetics",
-    "chemical-reactions",
-    "acids-bases-salts",
-    "periodic-table",
-    "metals",
-    "air-and-water",
-    "sulfur",
-    "carbonates",
-    "organic-chemistry",
+    "general-physics",
 ]
-SUBJECT = "chemistry"
+SUBJECT = "physics"
 
 #################################################################################
 topic_links = {}
 for t in TOPIC_LIST:
+    os.makedirs(f'{SUBJECT}/{t}', exist_ok=True)
     response = requests.get(
         f"https://www.physicsandmathstutor.com/{SUBJECT}-revision/igcse-cie/{t}/")
-    open(f"pastpapers/{SUBJECT}/{t}/index.html", "wb").write(response.content)
+    open(f"{SUBJECT}/{t}/index.html", "wb").write(response.content)
 
-    with open(f'pastpapers/{SUBJECT}/{t}/index.html') as f:
+    with open(f'{SUBJECT}/{t}/index.html') as f:
         found = False
         finished = False
         links = []
@@ -47,5 +35,5 @@ for t in TOPIC_LIST:
                     if not match.endswith('.pdf'):
                         continue
                     response = requests.get(match)
-                    open(f"pastpapers/{SUBJECT}/{t}/{os.path.basename(match)}",
+                    open(f"{SUBJECT}/{t}/{os.path.basename(match)}",
                          "wb").write(response.content)
